ff76baef1b7d9d4595d73879ba8d2cb539e849b7
danielnilssonjj <daniel.nilsson.jj@gmail.com>
2016-03-02 16:24:44 +0100

    Added target-following example

---
+++ simulink/Examples/pd_position_control_test/init_pd_controller.m
@@ -0,0 +1,3 @@
+% Parameters for PD-control
+KzP = 1.5; KetaP = 6*[1 1 1];
+KzD = 2.5; KetaD = 1.75*[1 1 1];
---
+++ simulink/Examples/pd_position_control_test/init_pd_position_controller.m
@@ -0,0 +1,6 @@
+Po = 0.08; % P-part of position controller
+Io = 0.0; % I-part of position controller
+Do = 0.08; % D-part of position controller
+N = 100; % Filter coeficcient for D-part
+
+seed = 1234; % Seed for random target generators
