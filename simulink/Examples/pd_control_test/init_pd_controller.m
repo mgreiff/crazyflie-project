de2ad6b5ef95ace1d2ddbc3c830fb1cdd51c0393
danielnilssonjj <daniel.nilsson.jj@gmail.com>
2016-03-02 23:01:58 +0100

    More fix

--- simulink/Examples/pd_control_test/init_pd_controller.m
+++ simulink/Examples/pd_control_test/init_pd_controller.m
@@ -1,3 +1,3 @@
 % Parameters for PD-control
-KzP = 1.5; KetaP = 6*[1 1 1];
-KzD = 2.5; KetaD = 1.75*[1 1 1];
+KzP = 1.5; KetaP = 8*[1 1 1];
+KzD = 2.5; KetaD = 4.5*[1 1 1];
--- simulink/Examples/pd_position_control_test/init_pd_position_controller.m
+++ simulink/Examples/pd_position_control_test/init_pd_position_controller.m
@@ -1,6 +1,6 @@
-Po = 0.08; % P-part of position controller
+Po = 0.2; % P-part of position controller
 Io = 0.0; % I-part of position controller
-Do = 0.08; % D-part of position controller
+Do = 0.17; % D-part of position controller
 N = 100; % Filter coeficcient for D-part
 
-seed = 1234; % Seed for random target generators
+seed = 123; % Seed for random target generators
