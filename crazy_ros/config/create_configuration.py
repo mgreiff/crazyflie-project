a36310bbb6ecf1ad631df2c16fe117b958294e05
Marcus Greiff <marcusgreiff.93@hotmail.com>
2016-03-05 02:38:25 +0100

    Updated configuration file, quadcopter model and dependencies
    The configuration file now defines the initial states of the quadcopter
    model in a 12x1 vector. The file can also be edited in the
    create_configuration script, in which the encoded JSON object is stored.
    
    The quadcopterModel node now includes the dynamics, which can be run
    separately in perfect time by executing the simulation script in /examples.
    However, in the current ROS implementation, the reference is passed trhough
    a topic, causing non-deterministic timelags and giving rise to a slightly
    messy simulation (though functional).
    
    The dependencies in the package XML were updated to accomodate the changes.

---
+++ crazy_ros/config/create_configuration.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import json
+
+param = {'global': {'timestep': 0.01},
+ 'quadcopter_model': {'A': [0.25, 0.25, 0.25],
+  'I': [0.004856, 0.004856, 0.008801],
+  'Im': 3.357e-05,
+  'b': 1.14e-07,
+  'g': 9.81,
+  'k': 2.98e-06,
+  'l': 0.225,
+  'm': 0.468,
+  'x_init': [[0.],[0.],[0.],[0.],[0.],[0.],[0.],[0.],[0.],[0.],[0.],[0.]]}}
+
+with open('configparam.cnf', 'w') as cnffile:
+    json.dump(param, cnffile, separators=(',', ':'),sort_keys=True, indent=4)
