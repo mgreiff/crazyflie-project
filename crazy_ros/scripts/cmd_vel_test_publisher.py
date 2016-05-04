f9d3fe14918476913e3ac2c3c64744783e27149b
Daniel Nilsson <daniel.nilsson.jj@gmail.com>
2016-05-04 13:58:48 +0200

    Added simple node to test thrusts on real copter

---
+++ crazy_ros/scripts/cmd_vel_test_publisher.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+
+class CmdVelTestPublisher(object):
+    def __init__(self):
+        self.p = rospy.Publisher('crazyflie/cmd_vel', Twist, queue_size = 10)
+        self.twist = Twist()
+        self.r = rospy.Rate(50)
+        
+        self.start_pub()
+    
+    def start_pub(self):
+        self.twist.linear.z = 40000
+        d = -4000/50
+        L = 60000
+        while not rospy.is_shutdown():
+            self.twist.linear.z += d
+            print 'linear.z = %d' % self.twist.linear.z
+            
+            if self.twist.linear.z >= L:
+                d = -d
+            if self.twist.linear.z < 10000:
+                self.stop()
+                break
+                    
+            self.p.publish(self.twist)
+            self.r.sleep()
+           
+    def stop(self):
+        self.twist.linear.z = 0
+        self.p.publish(self.twist)
+        
+
+if __name__ == '__main__':
+    rospy.init_node('cmd_vel_test_publisher')
+    cmdvel = CmdVelTestPublisher()
